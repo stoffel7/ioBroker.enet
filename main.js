279dfd1c3cdf0ffb424d19c5684817176e253529
Sebastian Schultz <mail@sebastian-schultz.de>
2018-01-03 11:42:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-/**
- *
- * template adapter
- *
- *
- *  file io-package.json comments:
- *
- *  {
- *      "common": {
- *          "name":         "enet",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.1",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Jung/Gira eNet Adapter",  // Adapter title shown in User Interfaces
- *          "authors":  [                               // Array of authord
- *              "SebastianSchultz <mail@sebastian-schultz.de>"
- *          ]
- *          "desc":         "Controls Jung/Gira eNet Smarthome devices through the eNet WLAN gateway",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
- *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "materialize":  true,                       // support of admin3
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
- *          "loglevel":     "info"                      // Adapters Log Level
- *      },
- *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "ip": "",
- *          "interval": "500"
- *      }
- *  }
- *
- */
+
 
 /* jshint -W097 */// jshint strict:false
 /*jslint node: true */
@@ -37,8 +9,8 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = new utils.Adapter('template');
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.enet.0
+var adapter = new utils.Adapter('enet');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
