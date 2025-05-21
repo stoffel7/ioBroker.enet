5c1d7ebdb8580bbbe8c8cbfd8334fbf25b52e42f
stoffel7 <39948331+stoffel7@users.noreply.github.com>
2025-05-21 19:50:28 +0200

    eslint config added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,11 @@
-// ioBroker eslint template configuration file for js and ts files
-// Please note that esm or react based modules need additional modules loaded.
-import config from '@iobroker/eslint-config';
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default [
-    ...config,
-    {
-        // specify files to exclude from linting here
-        ignores: [
-            '.dev-server/',
-            '.vscode/',
-            '*.test.js',
-            'test/**/*.js',
-            '*.config.mjs',
-            'build',
-            'admin/build',
-            'admin/words.js',
-            'admin/admin.d.ts',
-            'admin/blockly.js',
-            '**/adapter-config.d.ts',
-        ],
-    },
-    {
-        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
-        // as this improves maintainability. jsdoc warnings will not block buiuld process.
-        rules: {
-            // 'jsdoc/require-jsdoc': 'off',
-            // 'jsdoc/require-param-description': 'off',
-            // 'jsdoc/require-returns-description': 'off',
-            // 'jsdoc/require-returns-check': 'off',
-        },
-    },
-];
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
