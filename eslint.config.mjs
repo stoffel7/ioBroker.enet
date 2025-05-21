4638f56c2f4ba65d4d03fc01b799ecdf9f6c5895
stoffel7 <39948331+stoffel7@users.noreply.github.com>
2025-05-21 20:04:05 +0200

    eslint.config.mjs updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,47 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+//import js from "@eslint/js";
+//import globals from "globals";
+//import pluginReact from "eslint-plugin-react";
+//import { defineConfig } from "eslint/config";
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+//export default defineConfig([
+//  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+//  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+//  pluginReact.configs.flat.recommended,
+//]);
+// ioBroker eslint template configuration file for js and ts files
+// Please note that esm or react based modules need additional modules loaded.
+import config from '@iobroker/eslint-config';
+
+export default [
+    ...config,
+
+    {
+        // specify files to exclude from linting here
+        ignores: [
+            '.dev-server/',
+            '.vscode/',
+            '*.test.js', 
+            'test/**/*.js', 
+            '*.config.mjs', 
+            'build', 
+            'node_modules',
+            'admin/build', 
+            'admin/words.js',
+            'admin/admin.d.ts',
+            '**/adapter-config.d.ts'     
+        ] 
+    },
+
+    {
+        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
+        // as this improves maintainability. jsdoc warnings will not block buiuld process.
+        rules: {
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/no-defaults': 'off',
+            'jsdoc/require-param-description' : 'off',
+            '@typescript-eslint/no-unused-vars': 'off'
+        },
+    },
+    
+];
